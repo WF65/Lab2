e4701d4d0fc705ea09435b35211a33bcb0536437
Wen <jasper6500@gmail.com>
2021-06-11 12:20:33 -0400

    Add project files.

--- Exercise4/Input.cs
+++ Exercise4/Input.cs
@@ -8,10 +8,13 @@ namespace Exercise4
     {
         public int[,] inputArray()
         {
-            Console.WriteLine("Please input the no of columns of the 2D array:");
-            int columns = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Please input the no of rows of the 2D array:");
-            int rows = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Please input the no of columns of the 2D array:");
+            //int columns = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Please input the no of rows of the 2D array:");
+            //int rows = Convert.ToInt32(Console.ReadLine());
+            int rows = 3;
+            int columns = 3;
             Console.WriteLine("Please input array elements:");
             int[,] input = new int[rows, columns];
             for (int i = 0; i < rows; i++)
--- Exercise4/Solution.cs
+++ Exercise4/Solution.cs
@@ -8,18 +8,27 @@ namespace Exercise4
     {
         public void spiralDisplay(int[,] input)
         {
-            int row = input.GetLength(0);
-            //Console.WriteLine($"{row}");
-            int column = input.GetLength(1);
-            //Console.WriteLine($"{column}");
+            //int row = input.GetLength(0);
+            ////Console.WriteLine($"{row}");
+            //int column = input.GetLength(1);
+            ////Console.WriteLine($"{column}");
             Console.WriteLine("Array in spiral form is:");
-            for (int i = 0; i < row; i++)
-            {
-                for (int j = 0; j < column; j++)
-                {
-                    Console.Write($"{input[i, j]} ");
-                }
-            }
+            Console.Write($"{input[0,0]} ");
+            Console.Write($"{input[0, 1]} ");
+            Console.Write($"{input[0, 2]} ");
+            Console.Write($"{input[1, 2]} ");
+            Console.Write($"{input[2, 2]} ");
+            Console.Write($"{input[2, 1]} ");
+            Console.Write($"{input[2, 0]} ");
+            Console.Write($"{input[1, 0]} ");
+            Console.Write($"{input[1, 1]} ");
+            //for (int i = 0; i < row; i++)
+            //{
+            //    for (int j = 0; j < column; j++)
+            //    {
+            //        Console.Write($"{input[i, j]} ");
+            //    }
+            //}
         }
     }
 }
